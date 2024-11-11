324376cf7b01122c9990bdd886106a2e071f743e
José Brito <josealbritose@gmail.com>
2024-11-11 18:35:43 -0300

    styles

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -8,8 +8,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-w-[100vh] bg-white text-gray-800">
-      <div className="max-w-[1536px] mx-auto px-[80px]">
+    <div className="w-full bg-white text-gray-800">
+      <div className="mx-auto px-10">
         <Navbar />
         {children}
         <Footer />
