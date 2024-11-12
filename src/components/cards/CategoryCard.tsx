85759f33ac8bfbefafd54b0fcfece36cc638429d
José Brito <josealbritose@gmail.com>
2024-11-12 01:58:39 -0300

    CatogoryList

--- src/components/cards/CategoryCard.tsx
+++ src/components/cards/CategoryCard.tsx
@@ -1,28 +1,32 @@
 import * as React from "react";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryCardProps {
   card: {
     label: string;
     img: string;
-    className:string
+    className: string;
   };
 }
 export function CategoryCard({ card }: CategoryCardProps) {
   return (
-    <article className={`w-[150px] h-12 ${card.className} flex flex-row-reverse justify-center items-center gap-x-2 rounded-lg`}>
-      <h2 className="text-gray-800 font-semibold">{card.label}</h2>
-      <section className="w-8 h-8 overflow-hidden rounded-full">
-        <Image
-          alt={card.label}
-          src={card.img}
-          height={30}
-          width={30}
-          className="rounded-full"
-        />
-      </section>
-    </article>
+    <Link href={`/blog?cat=${card.label}`}>
+      <article
+        className={`w-[150px] h-12 ${card.className} flex flex-row-reverse justify-center items-center gap-x-2 rounded-lg transition-colors duration-200 ease-out`}
+      >
+        <h2 className="text-gray-800 font-semibold">{card.label}</h2>
+        <section className="w-8 h-8 overflow-hidden rounded-full">
+          <Image
+            alt={card.label}
+            src={card.img}
+            height={30}
+            width={30}
+            className="rounded-full"
+          />
+        </section>
+      </article>
+    </Link>
   );
 }
--- src/components/cards/CategoryList.tsx
+++ src/components/cards/CategoryList.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { CategoryCard } from "./CategoryCard";
 
+const categories = [
+  {
+    label: "Style",
+    img: "/images/style.png",
+    className: "bg-sky-100 hover:bg-sky-200",
+  },
+  {
+    label: "Fashion",
+    img: "/images/fashion.png",
+    className: "bg-purple-100 hover:bg-purple-200",
+  },
+  {
+    label: "Food",
+    img: "/images/food.png",
+    className: "bg-green-100 hover:bg-green-200",
+  },
+  {
+    label: "Travel",
+    img: "/images/travel.png",
+    className: "bg-rose-100 hover:bg-rose-200",
+  },
+  {
+    label: "Culture",
+    img: "/images/culture.png",
+    className: "bg-orange-100 hover:bg-orange-200",
+  },
+  {
+    label: "Coding",
+    img: "/images/coding.png",
+    className: "bg-violet-100 hover:bg-violet-200",
+  },
+];
 export default function CategoryList() {
   return (
-    <div>
-      CategoryList
-    </div>
-  )
+    <section className="flex flex-col  gap-8 mx-auto px-10">
+      <h1 className="mt-14 font-semibold text-xl">Popular Categories</h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 md:gap-8 lg:grid-cols-6 gap-5">
+        {categories.map((item) => (
+          <CategoryCard key={item.label} card={item} />
+        ))}
+      </div>
+    </section>
+  );
 }
