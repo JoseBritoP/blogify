84156118f33b1d7d680f42b705a5d68ebbbc81c9
José Brito <josealbritose@gmail.com>
2024-11-12 06:20:00 -0300

    Aside

---
+++ src/components/Aside.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MostPopularPost from "./MostPopularPost";
+import Categories from "./Categories";
+import BlogsByEditor from "./BlogsByEditor";
+
+export default function Aside() {
+  return (
+    <aside className="w-1/3 flex flex-col items-center justify-start gap-y-10">
+      <MostPopularPost />
+      <Categories />
+      <BlogsByEditor />
+    </aside>
+  );
+}
