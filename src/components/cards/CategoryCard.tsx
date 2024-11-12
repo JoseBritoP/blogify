1d3fbe04e5aedd62f04e8d4d2f6f6128d1a2bc47
José Brito <josealbritose@gmail.com>
2024-11-12 06:07:12 -0300

    Category Card

--- src/components/cards/CategoryCard.tsx
+++ src/components/cards/CategoryCard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 interface CategoryCardProps {
   card: {
     label: string;
-    img: string;
+    img?: string;
     className: string;
   };
 }
@@ -17,15 +17,29 @@ export function CategoryCard({ card }: CategoryCardProps) {
         className={`w-[150px] h-12 ${card.className} flex flex-row-reverse justify-center items-center gap-x-2 rounded-lg transition-colors duration-200 ease-out`}
       >
         <h2 className="text-gray-800 font-semibold">{card.label}</h2>
-        <section className="w-8 h-8 overflow-hidden rounded-full">
-          <Image
-            alt={card.label}
-            src={card.img}
-            height={30}
-            width={30}
-            className="rounded-full"
-          />
-        </section>
+        {card.img && (
+          <section className="w-8 h-8 overflow-hidden rounded-full">
+            <Image
+              alt={card.label}
+              src={card.img}
+              height={30}
+              width={30}
+              className="rounded-full"
+            />
+          </section>
+        )}
+      </article>
+    </Link>
+  );
+}
+
+export function SimpleCategoryCard({ card }: CategoryCardProps) {
+  return (
+    <Link href={`/blog?cat=${card.label}`}>
+      <article
+        className={`${card.className} p-2 rounded-xl`}
+      >
+        <h2 className="text-gray-800 font-semibold text-center">{card.label}</h2>
       </article>
     </Link>
   );
