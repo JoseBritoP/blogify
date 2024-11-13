d7c7c20becb7f72f06feba0ae00fae6e5ef58182
José Brito <josealbritose@gmail.com>
2024-11-13 03:43:24 -0300

    Text

--- src/components/BlogsByEditor.tsx
+++ src/components/BlogsByEditor.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { CardEditor } from './cards/CardEditor';
-import { blogs } from '@/constants';
-
+import React from "react";
+import { CardEditor } from "./cards/CardEditor";
+import { blogs } from "@/constants";
 
 export default function BlogsByEditor() {
   return (
-    <section className='grid grid-cols-1 gap-y-4 w-5/6'>
-      {blogs.map((blog)=>(
-        <CardEditor key={blog.category} card={blog}/>
-      ))}
+    <section className="grid grid-cols-1 gap-y-4 w-5/6">
+      <div>
+        <p className="text-slate-500 text-base">{"Chosen by the editor"}</p>
+        <h2 className="font-bold text-xl">Editors Pick</h2>
+      </div>
+      <div className="grid grid-cols-1 gap-y-4 w-full">
+        {blogs.map((blog) => (
+          <CardEditor key={blog.category} card={blog} />
+        ))}
+      </div>
     </section>
-  )
+  );
 }
