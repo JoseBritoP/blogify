151456aea860331e47b62197654aa225e178024b
José Brito <josealbritose@gmail.com>
2024-11-13 03:34:02 -0300

    Blogs

--- src/components/BlogsByEditor.tsx
+++ src/components/BlogsByEditor.tsx
@@ -5,7 +5,7 @@ import { blogs } from '@/constants';
 
 export default function BlogsByEditor() {
   return (
-    <section className='grid grid-cols-1 gap-y-4'>
+    <section className='grid grid-cols-1 gap-y-4 w-5/6'>
       {blogs.map((blog)=>(
         <CardEditor key={blog.category} card={blog}/>
       ))}
