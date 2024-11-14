36a6fb5b4d251b8c488b2ab86bbfed9516ca2765
José Brito <josealbritose@gmail.com>
2024-11-14 19:11:42 -0300

    styles

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -5,7 +5,7 @@ import BlogsByEditor from "./BlogsByEditor";
 
 export default function Aside() {
   return (
-    <aside className="w-1/3 flex flex-col items-center justify-start gap-y-10">
+    <aside className="w-full lg:w-1/3 flex flex-col items-center justify-start gap-y-10 bg-sky-100">
       <MostPopularPost />
       <Categories />
       <BlogsByEditor />
