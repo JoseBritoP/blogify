8f8858956348391bc5569911bd593317e6a8d3ab
José Brito <josealbritose@gmail.com>
2024-11-14 19:11:47 -0300

    styles

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,7 +4,7 @@ import Aside from "./Aside";
 
 export default function Menu() {
   return (
-    <main className="flex w-full gap-4 items-start mx-auto px-10">
+    <main className="flex flex-col lg:flex-row w-full gap-4 items-start mx-auto px-10">
       <RecentPost />
       <Aside />
     </main>
