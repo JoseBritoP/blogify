37e0adae463f43f7c9dcc5b267457799c52eaf66
José Brito <josealbritose@gmail.com>
2024-11-14 21:56:06 -0300

    Page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,18 +1,14 @@
-import CardList from '@/components/cards/CardList'
-import CategoryList from '@/components/cards/CategoryList'
-import Featured from '@/components/Featured'
-import Menu from '@/components/Menu'
-import React from 'react'
+import CategoryList from "@/components/cards/CategoryList";
+import Featured from "@/components/Featured";
+import Menu from "@/components/Menu";
+import React from "react";
 
 export default function HomePage() {
   return (
-    <main>
-      <Featured/>
-      <CategoryList/>
-      <section>
-        <CardList/>
-        <Menu/>
-      </section>
-    </main>
-  )
+    <>
+      <Featured />
+      <CategoryList />
+      <Menu />
+    </>
+  );
 }
