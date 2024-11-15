c2facadb4ef28f992b00fe39fd91dac7498fc5f7
José Brito <josealbritose@gmail.com>
2024-11-15 17:29:59 -0300

    Styles

--- src/components/aside/BlogsByEditor.tsx
+++ src/components/aside/BlogsByEditor.tsx
@@ -4,14 +4,14 @@ import { blogs } from "@/constants";
 
 export default function BlogsByEditor() {
   return (
-    <section className="flex flex-col w-full gap-y-4">
+    <section className="flex flex-col w-full gap-y-4 gap-x-2">
       <div>
         <p className="text-slate-600 text-base font-medium">
           {"Chosen by the editor"}
         </p>
         <h2 className="font-bold text-xl">Editors Pick</h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full lg:grid-cols-1">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full lg:grid-cols-2">
         {blogs.map((blog) => (
           <CardEditor key={blog.category} card={blog} />
         ))}
--- src/components/aside/MostPopularBlog.tsx
+++ src/components/aside/MostPopularBlog.tsx
@@ -9,7 +9,7 @@ export default function MostPopularBlog() {
         <p className="text-slate-500 text-base font-medium">{"What's hot"}</p>
         <h2 className="font-bold text-xl text-slate-800">Most Popular</h2>
       </div>
-      <div className="grid grid-cols-1 gap-y-2 md:gap-y-4 w-full pr-2 md:grid-cols-2 lg:grid-cols-1">
+      <div className="grid grid-cols-1 gap-y-2 md:gap-y-4 w-full pr-2 md:grid-cols-2 lg:grid-cols-2 gap-x-2">
         {blogs.map((blog) => (
           <CardPopularBlog key={blog.category} card={blog} />
         ))}
